d761391b60ec30d649847dca75431ce44dcc2b64
Alexander Malshakov <89681115653@mail.ru>
2023-06-15 16:21:14 +0300

    Change fin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,4 @@
 import os
-from distutils import util
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -8,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = bool(util.strtobool(os.getenv('DEBUG', default='False')))
+DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split(', ')
+ALLOWED_HOSTS = ['130.193.43.242', '127.0.0.1', 'localhost', 'amalshakov.hopto.org']
 
 
 INSTALLED_APPS = [
